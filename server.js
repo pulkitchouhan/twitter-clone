78913399fd1d2b8b809633b57ac61cb1c27725d0
Pulkit <pulkit@WIL-Admins-MacBook-Pro-2.local>
2024-12-27 13:36:52 +0530

    fix: vercel

--- server.js
+++ server.js
@@ -15,8 +15,10 @@ app.use(express.json());
 app.use(cors());
 
 // Routes
+app.use("/", (req, res) => {
+  res.json({ success: true, message: "Server running" });
+});
 app.use("/api/auth", authRoutes);
 app.use("/api/tweets", tweetRoutes);
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
